5b47762c71dcd9e62f6224bac728418d8ceff9ac
l0fty-WhizZ <shreyas@shitjustgotreal.in>
2016-08-28 19:29:12 +0530

    Added audio clips when aggression increases
    Audio clips like, violet thunder, demon laughing sounds were added, and
    a sound system for demon is also built. Tweaked the lighting for better
    visibility.

---
+++ Assets/Scripts/ThunderScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThunderScript : MonoBehaviour {
+	[SerializeField]
+	private GameObject demon;
+	[SerializeField]
+	private AudioClip[] demonSounds;
+	void Start () {
+			
+	}
+	
+	void Update () {
+		if (demon.GetComponent<Teleport> ().score >= 7) {
+			if (gameObject.name == "_DemonSoundSystem" && ((int)Time.timeSinceLevelLoad % 9 == 0) && (Time.timeSinceLevelLoad - (int)Time.timeSinceLevelLoad <= 0.04f)) {
+				if (GetComponent<AudioSource> ().isPlaying)
+					goto A;
+				else {
+					GetComponent<AudioSource> ().Play ();
+				}
+				A:
+				{}
+			} else if (gameObject.name != "_DemonSoundSystem") {
+				if (GetComponent<AudioSource> ().isPlaying)
+					goto B;
+				else {
+					GetComponent<AudioSource> ().Play ();
+				}
+				B:
+				{}
+			}
+		}
+	}
+}
